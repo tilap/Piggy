8367fb35872f342f1243abd5f29e155af3231669
Julien Tilap LA VINH <jlavinh@gmail.com>
2015-08-05 18:58:58 +0200

    new: module generation: add basic service template

---
+++ dev/modules/templates/Service.js
@@ -0,0 +1,18 @@
+import Service from 'piggy-module/lib/Service';
+
+export default class {{Nicename}}Service extends Service{
+
+  constructor(manager) {
+    super(manager);
+  }
+
+}
+import Service from 'piggy-module/lib/Service';
+
+export default class {{Nicename}}Service extends Service{
+
+  constructor(manager) {
+    super(manager);
+  }
+
+}
