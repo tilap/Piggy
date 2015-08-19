d716212704f10c7df5ce16bcbd7e19b751248fba
Julien Tilap LA VINH <jlavinh@gmail.com>
2015-08-19 19:20:03 +0200

    user controller clean

--- src/app/api/server/controllers/user.js
+++ src/app/api/server/controllers/user.js
@@ -1,5 +1,7 @@
+let serviceName = 'user';
 module.exports.get = function *() {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
+
   this.bag.setMultipleRessourceResponse();
 
   let options = this.utils.getFromQueryM(['page', 'limit', 'orderby', 'order'], null, true);
@@ -7,18 +9,21 @@ module.exports.get = function *() {
   let limit = options.limite && options.limit.match(/^\d+$/) && options.limit > 0 && options.limit < 1000 ? options.limit : 25;
   let orderby = options.orderby && ['username', 'firstname', 'lastname', 'email'].indexOf(options.orderby) > -1 ? options.orderby : 'username';
   let order = options.order && options.order === 'desc' ? 'desc' : 'asc';
-  let items = yield userService.getByPage({}, page, limit, orderby, order);
+
+  let items = yield service.getByPage({}, page, limit, orderby, order);
   this.bag.setDataFromVos(items);
   return this.renderBag();
 };
 
 
 module.exports.getOneById = function *() {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
 
   this.bag.setSingleRessourceResponse();
+
   let id = this.params.id || '';
-  let item = yield userService.getOneById(id);
+
+  let item = yield service.getOneById(id);
   this.assert(item, 404, 'item not found');
   this.bag.setDataFromVo(item);
   return this.renderBag();
@@ -26,11 +31,13 @@ module.exports.getOneById = function *() {
 
 
 module.exports.getOneByUsername = function *() {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
 
   this.bag.setSingleRessourceResponse();
+
   let username = this.params.username || '';
-  let item = yield userService.getOneByUsername(username);
+
+  let item = yield service.getOneByUsername(username);
   this.assert(item, 404, 'item not found');
   this.bag.setDataFromVo(item);
   return this.renderBag();
@@ -38,12 +45,13 @@ module.exports.getOneByUsername = function *() {
 
 
 module.exports.createOne = function *(next) {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
 
   this.bag.setSingleRessourceResponse();
+
   let itemData = this.utils.getFromPostM(['username', 'firstname', 'lastname', 'email']);
   try {
-    let newVo = yield userService.createNewOne(itemData, 'api');
+    let newVo = yield service.createNewOne(itemData, 'api');
     this.bag.setDataFromVo(newVo);
     this.renderBag();
   } catch(errors) {
@@ -57,16 +65,18 @@ module.exports.createOne = function *(next) {
 
 
 module.exports.updateOneById = function *() {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
 
   this.bag.setSingleRessourceResponse();
+
   let id = this.params.id || '';
-  let item = yield userService.getOneById(id);
+  let item = yield service.getOneById(id);
+
   this.assert(item, 404, 'item not found');
 
   let itemData = this.utils.getFromPostM(['firstname', 'lastname', 'email'], null, true);
   try {
-    let updatedVo = yield userService.updateOneFromData(itemData, item.id);
+    let updatedVo = yield service.updateOneFromData(itemData, item.id);
     this.bag.setDataFromVo(updatedVo);
     return this.renderBag();
   } catch(errors) {
@@ -79,15 +89,16 @@ module.exports.updateOneById = function *() {
 };
 
 module.exports.deleteOneById = function *() {
-  let userService = yield this.getModuleService('user');
+  let service = yield this.getModuleService(serviceName);
 
   this.bag.setRawResponse();
 
   let id = this.params.id || '';
-  let item = yield userService.getOneById(id);
+  let item = yield service.getOneById(id);
+
   this.assert(item, 404, 'item not found');
 
-  let successDeleted = yield userService.deleteOneById(item.id);
+  let successDeleted = yield service.deleteOneById(item.id);
   this.bag.setData({'deleted': successDeleted === true ? 1 : 0});
   return this.renderBag();
 };
